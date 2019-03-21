73090f31a59b54f1933a087e8c3a300385c97e6e
luohuidong <luohuidong01@126.com>
2019-03-21 23:07:22 +0800

    新增组件
    新增页面 Header 组件及 Tab 组件

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MHeader />
+    <Tab />
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MHeader from '@components/header/';
+import Tab from '@components/tab/';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MHeader,
+    Tab
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped lang="stylus">
+  @import "~@assets/stylus/variable"
+
+  #app
+    color: $color-theme
 </style>
