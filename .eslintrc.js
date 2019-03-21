81400b3c044b4ff02c2133e5816f4d09aa168e0b
luohuidong <luohuidong01@126.com>
2019-03-21 22:57:58 +0800

    eslint 配置更改

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,20 @@
 module.exports = {
-    "root": true,
-    "env": {
-        "node": true
-    },
-    "extends": [
-        "plugin:vue/essential",
-        "eslint:recommended"
-    ],
-    "rules": {
-        "indent": ["error", 2, {
-          "SwitchCase": 1
-        }],
-        "quotes": ["error", "single"],
-        "semi": ["error", "never"],
-        "require-await": "error",
-    },
-    "parserOptions": {
-        "parser": "babel-eslint"
-    }
-}
+  'root': true,
+  'env': {
+    'node': true
+  },
+  'extends': [
+    'plugin:vue/essential',
+  ],
+  'rules': {
+    'indent': ['error', 2, {
+      'SwitchCase': 1
+    }],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'require-await': 'error',
+  },
+  'parserOptions': {
+    'parser': 'babel-eslint'
+  }
+};
