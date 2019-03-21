a32ec498111fee6c6e4bb2a1448243c7ee87dd21
luohuidong <luohuidong01@126.com>
2019-03-21 22:58:21 +0800

    添加 proxy

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
-const path = require('path')
+const path = require('path');
 
 function resolve(dir) {
-  return path.resolve(__dirname, dir)
+  return path.resolve(__dirname, dir);
 }
 
 module.exports = {
@@ -11,8 +11,17 @@ module.exports = {
         '@components': resolve('src/components/'),
         '@assets': resolve('src/assets/'),
         '@api': resolve('src/api/'),
-        '@base': resolve('src/base/')
+        '@base': resolve('src/base/'),
+        '@pages': resolve('src/pages/')
       }
     }
+  },
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:3000',
+        pathRewrite: {'^/api' : ''},
+      },
+    }
   }
-}
+};
