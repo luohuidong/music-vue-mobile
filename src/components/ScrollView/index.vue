aede9cbdc3411e6589b37f92ceb5343a7ead38c5
luohuidong <luohuidong01@126.com>
2019-03-31 23:17:43 +0800

    新增滑动组件

---
+++ src/components/ScrollView/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <ScrollView ref="scrollView" :scrolling-x="false">
+    <slot></slot>
+  </ScrollView>
+</template>
+
+<script>
+import { ScrollView } from 'mand-mobile';
+
+export default {
+  components: {
+    ScrollView
+  },
+  props: {
+    data: {
+      types: Array,
+      default: () => []
+    }
+  },
+  watch: {
+    data() {
+      this.$refs.scrollView.reflowScroller();
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
