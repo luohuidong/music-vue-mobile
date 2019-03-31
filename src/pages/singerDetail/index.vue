6a1130d747f1f8f629af2196b3b6ddfe0f267215
luohuidong <luohuidong01@126.com>
2019-03-31 23:16:09 +0800

    新增歌手详情页面

---
+++ src/pages/singerDetail/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <MusicList :songs="songs" :picture="singer.picUrl" :title="singer.name"/>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions, mapGetters } from "vuex";
+import MusicList from "@components/MusicList";
+
+export default {
+  components: {
+    MusicList
+  },
+  computed: {
+    ...mapState("singerDetail", {
+      singer: state => state.singer
+    }),
+    ...mapGetters("singerDetail", ["songs"])
+  },
+  created() {
+    const { singerId } = this.$route.params;
+    this.handleGetSingerHotSong(singerId);
+  },
+  watch: {
+    '$route' (to) {
+      const { singerId } = to.params;
+      this.handleGetSingerHotSong(singerId);
+    }
+  },
+  methods: {
+    ...mapActions("singerDetail", ["handleGetSingerHotSong"])
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.container {
+  box-sizing: border-box;
+  height: 100vh;
+}
+</style>
