412da77c0ceffa7f61317e1f43014536d51e9158
luohuidong <luohuidong01@126.com>
2019-03-31 23:17:32 +0800

    新增歌曲列表组件

---
+++ src/components/MusicList/Picture/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container">
+    <div class="header">
+      <div class="icon" @click="handleIconClick">
+        <i class="icon-back"></i>
+      </div>
+
+      <div class="title">
+        <h1 class="title-text">{{ title }}</h1>
+      </div>
+    </div>
+
+    <img :src="picture" :alt="title">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    picture: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      defualt: ''
+    }
+  },
+  methods: {
+    handleIconClick() {
+      this.$router.go(-1);
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.container {
+  position: relative;
+  height: 300px;
+  overflow: hidden;
+
+  .header {
+    position: absolute;
+    width: 100%;
+    $height: 50px;
+
+    .icon {
+      position: absolute;
+      left: 15px;
+      height: $height;
+      display: flex;
+      align-items: center;
+    }
+
+    .title {
+      height: $height;
+      color: white;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+
+  img {
+    width: 100%;
+  }
+}
+</style>
---
+++ src/components/MusicList/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="music-list">
+    <Picture :picture="picture" :title="title" />
+    <div class="scroll-view-container">
+      <ScrollView :data="songs">
+        <SongList :songs="songs"/>
+      </ScrollView>
+    </div>
+  </div>
+</template>
+
+<script>
+import ScrollView from '@components/ScrollView/';
+import Picture from './Picture';
+import SongList from './SongList/';
+
+export default {
+  components: {
+    SongList,
+    Picture,
+    ScrollView
+  },
+  props: {
+    picture: {
+      type: String,
+      default: ''
+    },
+    songs: {
+      type: Array,
+      defualt: () => []
+    },
+    title: {
+      type: String,
+      defualt: ''
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.music-list {
+  height: 100%;
+  position: relative;
+}
+
+.scroll-view-container {
+  position: fixed;
+  top: 300px;
+  bottom: 0px;
+}
+</style>
