0ffd3ad8a5bc0ef82a4fd98d851862b64344b524
luohuidong <luohuidong01@126.com>
2019-03-31 23:12:20 +0800

    修改歌手列表路由

--- src/components/Tab/index.vue
+++ src/components/Tab/index.vue
@@ -3,7 +3,7 @@
     <router-link tag="div" class="tab-item" to="/recommend">
       <span class="tab-link">推荐</span>
     </router-link>
-    <router-link tag="div" class="tab-item" to="/singer">
+    <router-link tag="div" class="tab-item" to="/singer/list">
       <span class="tab-link">歌手</span>
     </router-link>
     <router-link tag="div" class="tab-item" to="/rank">
