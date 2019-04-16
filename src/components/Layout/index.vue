2f8e2417d111ff02eaf1b29131f12fec2b8edc73
luohuidong <luohuidong01@126.com>
2019-04-16 20:47:14 +0800

    将 Header 及 Tab 组件移至 Layout 组件中

--- src/components/Layout/index.vue
+++ src/components/Layout/index.vue
@@ -7,8 +7,8 @@
 </template>
 
 <script>
-import Header from '@components/Header';
-import Tab from '@components/Tab';
+import Header from './Header';
+import Tab from './Tab';
 
 export default {
   components: {
