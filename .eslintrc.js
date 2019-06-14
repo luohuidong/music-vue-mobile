fab8cce8f20ad481e6432371e18cbb0cc16ed010
luohuidong <luohuidong01@126.com>
2019-06-15 00:14:16 +0800

    修改引号规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     'indent': ['error', 2, {
       'SwitchCase': 1
     }],
-    'quotes': ['error', 'single'],
+    'quotes': ['error', 'double'],
     'semi': ['error', 'always'],
     'require-await': 'error',
   },
